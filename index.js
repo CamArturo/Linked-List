11ee2180f2b624893554e75c5f224c7222ab8042
Angela Deslandes <ang.deslandes@gmail.com>
2018-02-04 18:50:44 -0700

    Add files via upload
    new updated CSS to reflect mock

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-$(document).ready(function () {
-	$()
+
+function enterCol() {
+  var enter = document.getElementById("enter");
+
+  if(enter.style.display === 'none') {
+    enter.style.display = 'block';
+  } else {
+    enter.style.display = 'none';
+  }
+}
+
+$(document).ready(function() {
+    $()
+});
+
+$('#new-bookmark').click(function() {
+    var bookmarkTitle = this.title;
+    var bookmarkUrl = this.href;
+
+    if () {
+
+    } else {
+
+    }
 });
